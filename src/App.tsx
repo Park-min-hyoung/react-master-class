3f3e453b8e073d420e37f5111c9544f887826f43
Park-min-hyoung <qkralswgud1324@naver.com>
2022-07-12 17:35:11 +0900

    State

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import Circle from "./Circle";
 
 function App() {
+  const [value, setValue] = useState(0);
+  // Error 발생
+  // setValue("haha");
+  setValue(1);
   return (
     <div>
       <Circle borderColor="yellow" bgColor="teal" />
