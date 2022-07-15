ea60307c5356144b87c8aa03d3c8a00de5f8e2ae
Park-min-hyoung <qkralswgud1324@naver.com>
2022-07-15 11:00:12 +0900

    Price Chart

--- src/api.ts
+++ src/api.ts
@@ -15,3 +15,11 @@ export function fetchCoinTickers(coinId: string | undefined) {
     response.json()
   );
 }
+
+export function fetchCoinHistory(coinId: string | undefined) {
+  const endDate = Math.floor(Date.now() / 1000); // 현재 시간을 초로 나타냄
+  const startDate = endDate - 60 * 60 * 23 * 7 * 1; // 현재 시간에서 1주 -1 시간에 해당하는 초를 뺌;
+  return fetch(
+    `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
+  ).then((response) => response.json());
+}
